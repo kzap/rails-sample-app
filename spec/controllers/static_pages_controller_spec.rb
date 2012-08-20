de4eb8fdc7ce1d4025fce48fa5746fd744b3e506
Andre Marcelo-Tanner <andre@enthropia.com>
2012-08-21 06:12:42 +0800

    - added shims - added match routes - added contact page

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -23,4 +23,15 @@ describe StaticPagesController do
     end
   end
 
+  describe "GET 'contact'" do
+    it "returns http success" do
+      get 'contact'
+      response.should be_success
+    end
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1',
+				text: 'Contact')
+    end
+  end
 end
